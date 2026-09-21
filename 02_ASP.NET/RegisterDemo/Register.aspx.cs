7419e507e2517ea36cde3377d544f70da3a91d68
Bharvadiya Aaryan V <aryanbharvadiya93@gmail.com>
2026-09-21 23:00:53 +0530

    changes in Register file

--- 02_ASP.NET/RegisterDemo/Register.aspx.cs
+++ 02_ASP.NET/RegisterDemo/Register.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace RegisterDemo
 {
@@ -16,7 +17,15 @@ namespace RegisterDemo
 
         protected void btnRegister_Click(object sender, EventArgs e)
         {
-
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"D:\\R.K Univesrsity\\Sem-5\\.NET_Learning\\02_ASP.NET\\RegisterDemo\\App_Data\\RegisterDB.mdf\";Integrated Security=True";
+            SqlConnection con = new SqlConnection(connectionString);
+            //con.Open();
+            string query = "INSERT INTO Table Values('"+Name+"','"+Email+"','"+txtPassword+"')";
+            SqlCommand cmd = new SqlCommand(query,con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Response.Write("<script>alert('Registered Successfully');</script>");
+            con.Close();
         }
     }
 }
